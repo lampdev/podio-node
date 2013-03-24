36a1f89db745f2c0267fe298b0b0009ce7c74525
LAMPDev <orders@lamp-dev.com>
2013-03-24 09:15:25 +0100

    * removed credentials from demo script

--- demo.js
+++ demo.js
@@ -1,9 +1,9 @@
 var podio = require('./lib/main');
 
 try {
-    podio.init({client_secret: 'lDi0X4ZfS6c4nF536CJfSRaXKkxhN0mLTUPN8Q908Pn8hBoPnkOyyJQVtM53y2EI', client_id: 'lampdev-sample-app', app_token: 'bd35b64bb8924201b12909f5eeb267f4', app_id: '3788374', grant_type: 'app'});
+    podio.init({client_secret: 'client_secret', client_id: 'client_id', app_token: 'app_token', app_id: 'app_id', grant_type: 'app'});
     podio.authenticate(function(){
-        podio.addItem('3788374',{'title': 'Test project', 'description': 'description value'},function(item_id) {
+        podio.addItem('app_id',{'title': 'Test project', 'description': 'description value'},function(item_id) {
             console.log('Item created: ' + item_id);
             podio.uploadFile('../unnamed.png','nexus.png','image/png',function(file_id) {
                 console.log('File Uploaded: ' + file_id);
